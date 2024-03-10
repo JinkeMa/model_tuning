b6075fd14427369436415f2238b2a32da2796730
Tink <1243941867@qq.com>
2024-03-10 22:11:12 +0800

    自定义数据集子类

--- train/train.py
+++ train/train.py
@@ -6,30 +6,39 @@ import torch.optim as Optimum
 
 import torchvision.datasets as Datasets
 import torchvision.transforms as Transforms
+import torchvision.io as tio
 
 from torchvision.transforms import ToTensor, Lambda, Compose
 
-
-
 import os
+import cv2
+
 # 全局变量
 data_path = {
-    'train' : '../data/viod/train/',
-    'test'  : '../data/viod/test/'
+    'train' : 'F:/model_tuning/data/viod/train',
+    'test'  : 'F:/model_tuning/data/viod/test'
 }
-# 读取目录下的.json文件与对应的.jpg图像
-def read_json_image(data_path):
+
+# 读取目录下的jpg原图像与同名png掩膜图像
+def read_image(data_path):
     # 判断目录是否存在
     if not os.path.exists(data_path):
         raise FileNotFoundError('文件夹不存在')
-    # 读取目录下的jpg与json文件
-    img_files = os.listdir(data_path)
+    # 读取目录下的所有jpg图像与png图像
+    image_paths = [os.path.join(data_path, f) for f in os.listdir(data_path) if f.endswith('.jpg')]
+    # 用torchvision库读取image_paths里所有的图像
+    images = [tio.read_image(f) for f in image_paths]
+    # 将Tensor作为图像展示
+    print(images[0].shape)
+    cv2.imshow('image', images[0].permute(1,2,0).numpy())
+    cv2.waitKey(0)
     
 
+# 自定义数据集类DataSet
+class MyDataSet(Datasets.VisionDataset):
+    def __init__(self, root_dir, transform=None, target_transform=None):
+        super(MyDataSet, self).__init__(root_dir, transform, target_transform)
 
-# 加载unet网络的标注数据
-train_data = Datasets.ImageFolder(root='../data/viod/train/', transform=ToTensor())
-test_data = Datasets.ImageFolder(root='../data/viod/test/', transform=ToTensor())
-# 加载训练集和测试集
-train_loader = Data.DataLoader(dataset=train_data, batch_size=16, shuffle=True)
-test_loader = Data.DataLoader(dataset=test_data, batch_size=16, shuffle=False)
+    
+if __name__ == '__main__':
+    read_image(data_path['train'])
